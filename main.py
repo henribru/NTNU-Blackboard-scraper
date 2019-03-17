16465b16fafb901d906b3d0fbc1923686713271a
Nicholas Braaten <28822845+nicbra@users.noreply.github.com>
2019-03-18 00:51:40 +0100

    Changed main.py
    Made classes for courses and settings, as well as for avaliable content for download.

--- main.py
+++ main.py
@@ -4,34 +4,60 @@
 from bs4 import BeautifulSoup as bs
 from bs4 import Comment
 import requests
+import os
+import unicodedata
+import string
 
-s = None # Global session-variabel
+#s = None # Global session-variabel
 
-def login_settings():
+class GlobalSettings():
+	def __init__(self):
+		self.rootDir = os.path.abspath(os.path.curdir)
+		self.downloadDir = os.path.join(self.rootDir, 'Nedlastinger')
+		self.session = requests.Session()
 
-    username = input('Brukernavn: ')
-    password = input("Passord: ")
-    return {
-        'user_id' : username,
-        'password': password,
-        'login':'Logg på',
-        'action':'login',
-        'new_loc':'',
-    }
+class Courses():
+	def __init__(self, id, number, semester, kode, name):
+		self.semester = semester
+		self.id = id
+		self.name = name
+		self.nr = number	# Kønummer
+		self.kode = kode	# Emnekode
+
+class Content():
+	def __init__(self):
+		self.courseId
+		self.contentId
+
+
+s = GlobalSettings()
+
+
+def login_settings(username=None, password=None):	# Husk å endre dette!
+	if not(username and password):
+		username = input("Brukernavn: ")
+		password = input("Passord: ")
+	return {
+		'user_id' : username,
+		'password': password,
+		'login':'Logg på',
+		'action':'login',
+		'new_loc':'',
+	}
 
 def login():
-	global s
+	#global s
 	url = "https://ntnu.blackboard.com/webapps/login/" # URL for innlogging
 
 	login_data = login_settings()
 
 
 
-	s = requests.Session()
-	r = s.get(url) # Henter nettsiden
+	#s = requests.Session()
+	r = s.session.get(url) # Henter nettsiden
 	soup = bs(r.content, 'html5lib')
 	login_data['blackboard.platform.security.NonceUtil.nonce'] = soup.find('input', attrs={'name': 'blackboard.platform.security.NonceUtil.nonce'})['value']
-	r = s.post(url, data=login_data) # Send innlogging til blackboard
+	r = s.session.post(url, data=login_data) # Send innlogging til blackboard
 	soup = bs(r.content, 'html5lib')
 	fullt_navn = soup.find(id='global-avatar').next_element.strip()  # Henter ut fullt navn
 
@@ -39,13 +65,13 @@ def login():
 	return soup
 
 def searchInString(string, startString, stopString):
-    startIndex = string.find(startString)
-    stopIndex = string.find(stopString, startIndex)
+	startIndex = string.find(startString)
+	stopIndex = string.find(stopString, startIndex)
 
-    return string[startIndex+len(startString):stopIndex]
+	return string[startIndex+len(startString):stopIndex]
 
 def getCourseList():
-	global s
+	#global s
 	soup = login()
 	ajaxRefresh = {
 		'action': 'refreshAjaxModule',
@@ -56,20 +82,20 @@ def getCourseList():
 		if comment.strip() in['extid:learning/advancedcourses:']:
 			updateAjaxScript = str(comment.find_next('script'))
 
-    # Henter ut riktig form data for å requeste innhold i modulen
+	# Henter ut riktig form data for å requeste innhold i modulen
 	updateAjaxScript = searchInString(updateAjaxScript, 'refreshAjaxModule', '\n') # Korte ned på søkevariabelen
 	ajaxRefresh['modId'] = searchInString(updateAjaxScript, 'modId=', '&')
 	ajaxRefresh['tabId'] = searchInString(updateAjaxScript,'tabId=','&')
 	ajaxRefresh['tab_tab_group_id'] = searchInString(updateAjaxScript,'tab_tab_group_id',"'")
 
 	# Poster data for å requeste innholdet i modulen
-	rawCourseList = s.post('https://ntnu.blackboard.com/webapps/portal/execute/tabs/tabAction', data = ajaxRefresh)
+	rawCourseList = s.session.post('https://ntnu.blackboard.com/webapps/portal/execute/tabs/tabAction', data = ajaxRefresh)
 	rawCourseList=  bs(rawCourseList.content, 'html5lib') # Behandle motatt data med beautiful soup
 
 	#Sortere etter semester:
 	teller = 1
 	courses = rawCourseList.find_all(class_='termHeading-coursefakeclass')
-	emner = []
+	emner = {} # Dict av emneobjekter
 	for sem in courses:
 		semester = sem.span.next_sibling.strip()
 		emne = sem.find_next('div')
@@ -79,7 +105,8 @@ def getCourseList():
 			emnekode = streng[:streng.find(' ')].strip()
 			navn = streng[streng.find(' '):streng.find('(')-1].strip()
 			courseId = searchInString(str(a.get('href')), 'id=', '&')
-			emner.append((emnekode, semester, navn, courseId, teller)) # Lagre i tuppel
+			emner[teller] = Courses(courseId, teller, semester, emnekode, navn) # Lagre som objekt i dictionary
+			#emner.append((emnekode, semester, navn, courseId, teller)) # Lagre i tuppel
 			teller += 1
 
 	return emner
@@ -89,44 +116,69 @@ def printCourseList(emner):
 
 	print('nr'.ljust(4)+ 'Emnekode'.ljust(12) + 'Semester'.ljust(12)+'Emne'.ljust(50))
 	print('-'*75)
-	for j in emner:
-		print(str(j[4]).ljust(3) + '|' + j[0].ljust(12) + j[1].ljust(12) + j[2].ljust(50))
+	#for j in emner:
+	#	print(str(j[4]).ljust(3) + '|' + j[0].ljust(12) + j[1].ljust(12) + j[2].ljust(50))
+	for k in emner:
+		print(str(emner[k].nr).ljust(3) + '|' + emner[k].kode.ljust(12) + emner[k].semester.ljust(12) + emner[k].name.ljust(50))
 	print('-'*75)
 
 # Spør hvilke emner skal lastes ned
 def consoleCourseList(emner):
-	antEmner = len(emner)
-
 	print("List opp emnene du vil laste ned. 0 = alle\nEksempel: 1 3 9 10 15")
 	choise = input("Valg: ")
 	queue = []
-	if choise == "0":
-		for i in range(antEmner):
-			queue.append(i)
-	else:
-		for j in [i.strip() for i in choise.split(' ')]:
-			queue.append(int(j)-1)
+
+	for k in emner:
+		if (str(k) in [i.strip() for i in choise.split(' ')]) or (choise == '0'):	# Bare ta vare på gyldige instanser
+			queue.append(emner[k])
 
 	return queue
-
 # Hente innholdsstruktur i valgte emner
 def getCourseTree(id):
 	parameters = {
 		'initTree': 'true',
 		'storeScope': 'Session',
+		'expandAll': 'true',
 		'course_id': id,
 		'displayMode': 'courseMenu_newWindow',
 		'editMode': 'false',
 		'openInParentWindow': 'true'
 	}
-	response = s.post('https://ntnu.blackboard.com/webapps/blackboard/execute/course/menuFolderViewGenerator', data = parameters)
+	response = s.session.post('https://ntnu.blackboard.com/webapps/blackboard/execute/course/menuFolderViewGenerator', data = parameters)
+	return response
 
-courses = getCourseList()
-printCourseList(courses)
+
+## Lager gyldige filnavn
+def makeValidFilename(filename):
+	whitelist = "-_.()æøåÆØÅ %s%s" % (string.ascii_letters, string.digits)
+	char_limit = 255
+	# Bytt ut mellomrom
+	filename = filename.replace(' ','_')
+	
+	
+	cleanFilename = unicodedata.normalize('NFKD', filename).encode('UTF-8', 'ignore').decode()
+	
+	# Behold aksepterte tegn
+	cleanFilename = ''.join(c for c in cleanFilename if c in whitelist)
+
+	return cleanFilename[:char_limit]
+
+
+# Skrive data til fil
+def printToFile(directory, filename, data):
+	filename = makeValidFilename(filename)
+	path = os.path.join(directory, filename) 
+	with open(path, 'wb') as file:
+		file.write(data)
+
+
+courses = getCourseList()	# Hent et array med tuppler av alle emner
+printCourseList(courses)	# Skriv ut liste med alle emner
 FIFO = consoleCourseList(courses) # Opprette en kø av valgte emner
 
+os.makedirs(s.downloadDir, exist_ok=True)
 # DEQUEUE
 while len(FIFO) > 0:
 	pop = FIFO.pop(0)
-	id = courses[pop][3]
-	getCourseTree(id)
+	tree = getCourseTree(pop.id)
+	printToFile(s.downloadDir, pop.name+".txt", tree.content)
